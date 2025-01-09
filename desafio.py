f7ce362db93fa8c2e9ab59bebd6942c57f905029
rafael <rafael.ribeiro@a4pm.com.br>
2025-01-09 17:08:06 -0300

    Ajustando nome das variáveis

--- desafio.py
+++ desafio.py
@@ -3,6 +3,6 @@ nome_usuario = input("Informe o nome: ")
 salario_usuario = float(input("Informe o salário: "))
 bonus_usuario = float(input("Digite o valor do seu bônus: "))
 
-bonificacao = contante_bonus + salario * bonus
+bonificacao = contante_bonus + salario_usuario * bonus_usuario
 
-print(f"Sua bonificação : {bonificacao}")
+print(f"A bonificação do {nome_usuario} é de : {bonificacao}")
