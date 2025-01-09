e03a0ecdd8760e5d9f02898abbfd6eaf8399237a
rafael <rafael.ribeiro@a4pm.com.br>
2025-01-09 16:31:46 -0300

    Desenvolvendo calculadora e contar quantidade de caracteres apartir de uma entrada

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,16 @@
-print('Hello word')
+# print('Hello Word')
+# print(len(input("Digite o seu nome: ")))
+
+""""
+numero1 = int(input('Informe o primeiro número: '))
+numero2 = int(input('Informe o segundo número: '))
+operacao = input('Informe uma operação: ')
+
+if operacao == '+':
+    total = numero1 + numero2
+    print(total)
+elif operacao == '-':
+    total = numero1 - numero2
+    print(total) 
+
+"""
